9b13855398cd84acabf699a21b22a6b0a62df185
Bhupender <official.soni532@gmail.com>
2022-02-20 21:09:20 +0530

    completed single blog page

---
+++ News/news.js
@@ -0,0 +1,29 @@
+let headerParallax = document.getElementById("header_bg");
+console.log(headerParallax);
+
+window.addEventListener("scroll", () => {
+  let val = window.pageYOffset * 0.5;
+  headerParallax.style.backgroundPositionY = "-" + val + "px";
+});
+
+
+//!---------------------Pagination---------------
+let pagination = document.getElementById("pagi");
+let pagLi = pagination.querySelectorAll("li > a");
+pagLi.forEach((el) => {
+  el.addEventListener("click", () => {
+    el.classList.add("pag-active");
+    // console.log(el)
+  });
+});
+
+let doc = document.body;
+doc.addEventListener("click", (e) => {
+  let pagTarget = e.target.classList.contains("pag-btn");
+  if (!pagTarget) {
+    pagLi[0].classList.remove("pag-active");
+    pagLi[1].classList.remove("pag-active");
+    pagLi[2].classList.remove("pag-active");
+    pagLi[3].classList.remove("pag-active");
+  }
+});
